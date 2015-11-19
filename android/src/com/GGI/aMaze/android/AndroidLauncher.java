c27d3ce3d5b9672601f321ed5cbf0e0c995c7f2c
Emmett <goodgameindustries@gmail.com>
2015-11-19 17:41:06 -0600

    Project setup

---
+++ android/src/com/GGI/aMaze/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.GGI.aMaze.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.GGI.aMaze.aMaze;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new aMaze(), config);
+	}
+}
